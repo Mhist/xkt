ba2c2f64535da8e46aa04a116a270f393ab10284
mhist <18827637106@163.com>
2022-04-04 20:27:18 +0800

    更新

---
+++ docs/.vitepress/dist/assets/template_vue3常用模板.md.edd1efd7.js
@@ -0,0 +1,14 @@
+import{_ as e,c as t,o as a,a as r}from"./app.e3cca51f.js";const v='{"title":"\u7528\u4E8E\u8BB0\u5F55vue3\u5F00\u53D1\u4E2D\u5E38\u7528\u7684\u6A21\u677F","description":"","frontmatter":{},"headers":[{"level":2,"title":"store","slug":"store"}],"relativePath":"template/vue3\u5E38\u7528\u6A21\u677F.md"}',n={},o=r(`<h1 id="\u7528\u4E8E\u8BB0\u5F55vue3\u5F00\u53D1\u4E2D\u5E38\u7528\u7684\u6A21\u677F" tabindex="-1">\u7528\u4E8E\u8BB0\u5F55vue3\u5F00\u53D1\u4E2D\u5E38\u7528\u7684\u6A21\u677F <a class="header-anchor" href="#\u7528\u4E8E\u8BB0\u5F55vue3\u5F00\u53D1\u4E2D\u5E38\u7528\u7684\u6A21\u677F" aria-hidden="true">#</a></h1><h2 id="store" tabindex="-1">store <a class="header-anchor" href="#store" aria-hidden="true">#</a></h2><div class="language-"><pre><code>import {createStore} from &#39;vuex&#39;
+
+export default createStore({
+    state: {
+        
+    },
+    mutations: {
+
+    },
+    modules:{
+
+    }
+})
+</code></pre></div>`,3),s=[o];function _(c,d,i,l,u,h){return a(),t("div",null,s)}var f=e(n,[["render",_]]);export{v as __pageData,f as default};
---
+++ docs/template/vue3常用模板.md
@@ -0,0 +1,19 @@
+# 用于记录vue3开发中常用的模板
+
+## store
+```
+import {createStore} from 'vuex'
+
+export default createStore({
+    state: {
+        
+    },
+    mutations: {
+
+    },
+    modules:{
+
+    }
+})
+```
+
