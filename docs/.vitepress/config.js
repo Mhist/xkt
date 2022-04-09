2912a9f783bdebb82fb2ad75911854aee28cd013
mhist <18827637106@163.com>
2022-04-09 10:29:53 +0800

    favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,4 +4,8 @@ module.exports = {
   title: '亚东 VitePress',
   description: '前端知识体系',
   base: '/',
+  head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico' }],
+    // ['link', { rel: 'icon', type: 'image/svg+xml', href: '/path/to/icon.svg' }], for svg
+  ],
 }
