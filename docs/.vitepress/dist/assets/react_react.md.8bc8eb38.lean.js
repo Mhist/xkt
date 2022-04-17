db470ccc6ca33f0c49bae1981ab0ccab062a5d60
mhist <18827637106@163.com>
2022-04-17 23:28:34 +0800

    面试

--- docs/.vitepress/dist/assets/react_react.md.8bc8eb38.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as a,d as t}from"./app.6b72d739.js";const g='{"title":"\u521D\u8BC6react","description":"","frontmatter":{},"headers":[],"relativePath":"react/react.md"}',o={},c=t("",3),n=[c];function i(s,_,l,p,d,h){return a(),r("div",null,n)}var m=e(o,[["render",i]]);export{g as __pageData,m as default};
---
+++ docs/.vitepress/dist/assets/vitepress_index.md.1fae9958.js
@@ -0,0 +1,18 @@
+import{_ as e,c as t,o as a,b as n}from"./app.04e32d6d.js";const v='{"title":"vitepress \u914D\u7F6E\u8E29\u5751\u6307\u5357","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5BFC\u822A\u680F\u914D\u7F6E","slug":"\u5BFC\u822A\u680F\u914D\u7F6E"},{"level":2,"title":"\u96C6\u6210 element-plus \u81EA\u5B9A\u4E49\u7EC4\u4EF6","slug":"\u96C6\u6210-element-plus-\u81EA\u5B9A\u4E49\u7EC4\u4EF6"},{"level":2,"title":"\u54D4\u54E9\u54D4\u54E9\u5D4C\u5165 vitepress","slug":"\u54D4\u54E9\u54D4\u54E9\u5D4C\u5165-vitepress"}],"relativePath":"vitepress/index.md"}',s={},r=n(`<h1 id="vitepress-\u914D\u7F6E\u8E29\u5751\u6307\u5357" tabindex="-1">vitepress \u914D\u7F6E\u8E29\u5751\u6307\u5357 <a class="header-anchor" href="#vitepress-\u914D\u7F6E\u8E29\u5751\u6307\u5357" aria-hidden="true">#</a></h1><h2 id="\u5BFC\u822A\u680F\u914D\u7F6E" tabindex="-1">\u5BFC\u822A\u680F\u914D\u7F6E <a class="header-anchor" href="#\u5BFC\u822A\u680F\u914D\u7F6E" aria-hidden="true">#</a></h2><p>\u5D4C\u5957\u5BFC\u822A\u680F</p><div class="language-"><pre><code>const nav = [
+  {
+    text: &quot;\u5F00\u59CB&quot;,
+    link: &#39;start.html&#39;,
+    items: [
+      {
+        text: &quot;java&quot;,
+        link: &#39;java/index.html&#39;,
+      }
+    ]
+  },
+  {
+    text: &quot;vue3&quot;,
+    link: &#39;vue3/xiaotuxian.html&#39;,
+  },
+]
+export default nav
+</code></pre></div><div class="danger custom-block"><p class="custom-block-title">nested nav</p><p>\u8BBE\u7F6E\u5D4C\u5957\u94FE\u63A5 \u8BF7\u7528 items \u4EE3\u66FF children</p></div><h2 id="\u96C6\u6210-element-plus-\u81EA\u5B9A\u4E49\u7EC4\u4EF6" tabindex="-1">\u96C6\u6210 element-plus \u81EA\u5B9A\u4E49\u7EC4\u4EF6 <a class="header-anchor" href="#\u96C6\u6210-element-plus-\u81EA\u5B9A\u4E49\u7EC4\u4EF6" aria-hidden="true">#</a></h2><p><a href="https://segmentfault.com/a/1190000041599324#item-3-9" target="_blank" rel="noopener noreferrer">\u96C6\u6210 element-plus \u81EA\u5B9A\u4E49\u7EC4\u4EF6</a></p><h2 id="\u54D4\u54E9\u54D4\u54E9\u5D4C\u5165-vitepress" tabindex="-1">\u54D4\u54E9\u54D4\u54E9\u5D4C\u5165 vitepress <a class="header-anchor" href="#\u54D4\u54E9\u54D4\u54E9\u5D4C\u5165-vitepress" aria-hidden="true">#</a></h2>`,8),i=[r];function l(d,o,p,c,h,u){return a(),t("div",null,i)}var m=e(s,[["render",l]]);export{v as __pageData,m as default};
